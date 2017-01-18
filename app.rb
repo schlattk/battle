d79cad87c233c42f1ce621f8261dd7f0193667fb
konradschlatte <konrad.schlatte@btinternet.com>
2017-01-18 11:58:35 +0000

    step 19

--- app.rb
+++ app.rb
@@ -2,17 +2,26 @@ require 'sinatra/base'
 require 'sinatra'
 
 class Battle < Sinatra::Base
+
+enable :sessions
+
   get '/' do
-    @name1 = params[:name1]
-    @name2 = params[:name2]
+
     erb :index
   end
 
   post '/names' do
-    @name1 = params[:name1]
-    @name2 = params[:name2]
+    session[:name1] = params[:name1]
+    session[:name2] = params[:name2]
+    redirect "/play"
+  end
+
+  get '/play' do
+    @name1 = session[:name1]
+    @name2 = session[:name2]
     erb :names
   end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
