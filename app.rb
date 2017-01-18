ea04aaab4d1ab46568ea830e2c3f235dbf6eee92
Mike Field-May <mfieldmay@gmail.com>
2017-01-18 13:08:56 +0000

    Added viewing player HP

--- app.rb
+++ app.rb
@@ -19,6 +19,7 @@ enable :sessions
   get '/play' do
     @name1 = session[:name1]
     @name2 = session[:name2]
+    @hit_points = 20
     erb :names
   end
 
--- spec/features/capybara_feature_test_spec.rb
+++ spec/features/capybara_feature_test_spec.rb
@@ -8,7 +8,15 @@ RSpec.feature "Capybara feature test" do
     fill_in :name1, with: "Kate"
     fill_in :name2, with: "Konrad"
     click_button "Submit"
-    
+
     expect(page).to have_content "First player: Kate; second player: Konrad"
+  end
+  scenario "so players can see their Hit Points" do
+    visit "/"
+    fill_in :name1, with: "Kate"
+    fill_in :name2, with: "Konrad"
+    click_button "Submit"
+
+    expect(page).to have_content "Kate HP: 20, Konrad HP: 20"
   end
 end
