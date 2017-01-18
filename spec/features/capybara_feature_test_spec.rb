1793d67b9f268af109a739e63387095a7b256b98
konradschlatte <konrad.schlatte@btinternet.com>
2017-01-18 13:19:34 +0000

    stage 21

--- spec/features/capybara_feature_test_spec.rb
+++ spec/features/capybara_feature_test_spec.rb
@@ -3,19 +3,14 @@ require 'spec_helper'
 
 RSpec.feature "Capybara feature test" do
   scenario "players can enter their names and see them on screen" do
-    visit "/"
 
-    fill_in :name1, with: "Kate"
-    fill_in :name2, with: "Konrad"
-    click_button "Submit"
+    sign_in_and_play
 
     expect(page).to have_content "First player: Kate; second player: Konrad"
   end
   scenario "so players can see their Hit Points" do
-    visit "/"
-    fill_in :name1, with: "Kate"
-    fill_in :name2, with: "Konrad"
-    click_button "Submit"
+
+    sign_in_and_play
 
     expect(page).to have_content "Kate HP: 20, Konrad HP: 20"
   end
---
+++ spec/features/web_helpers.rb
@@ -0,0 +1,10 @@
+
+def sign_in_and_play
+
+  visit "/"
+
+  fill_in :name1, with: "Kate"
+  fill_in :name2, with: "Konrad"
+  click_button "Submit"
+
+end
