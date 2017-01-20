9f709d858cef13506f84de0bf9128bbdcef14ec6
Ross Benzie <rossbenzie@Ross-MacBook-Pro.local>
2017-01-20 12:25:56 +0000

    Replaced global var with class var

--- app.rb
+++ app.rb
@@ -13,27 +13,28 @@ enable :sessions
   end
 
   post '/names' do
-    $game = Game.new(Player.new(params[:name1]),Player.new(params[:name2]))
+    @@game = Game.new(Player.new(params[:name1]),Player.new(params[:name2]))
     redirect "/play"
   end
 
   get '/play' do
-    @name1 = $game.player1.name
-    @name2 = $game.player2.name
-    @hit_points = $game.player1.hit_points
+    @name1 = @@game.player1.name
+    @name2 = @@game.player2.name
+    @hit_points = @@game.player1.hit_points
     erb :names
   end
 
   get '/attack' do
-    @player1 = $game.player1
-    @player2 = $game.player2
+    # @@game = Game.instance(params[:player1], params[:player2])
+    @player1 = @@game.player1
+    @player2 = @@game.player2
     @name1 = @player1.name
     @name2 = @player2.name
-    @attacker = $game.attacker.name
-    @victim = $game.victim.name
-    $game.attacks
-    $game.switch_players
-    @game_over = $game.over? 
+    @attacker = @@game.attacker.name
+    @victim = @@game.victim.name
+    @@game.attacks
+    @@game.switch_players
+    @game_over = @@game.over?
     @p1_hp = @player1.hit_points
     @p2_hp = @player2.hit_points
     erb :attack
